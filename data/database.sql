d38ff22af9b6e31e303bc27e8ab76f4174ec3d36
Chloe Cahuzac <chloe.cahuzac@2020.icam.fr>
2021-04-11 19:13:24 +0200

    suppression anciens commentaires

--- data/database.sql
+++ data/database.sql
@@ -1,43 +1,13 @@
--- CREATE TABLE IF NOT EXISTS DossiersSource (
---     idDossierSource INTEGER PRIMARY KEY AUTOINCREMENT, 
---     cheminDossierSource TEXT NOT NULL
--- );
-
--- CREATE TABLE IF NOT EXISTS DossiersDestination (
---     idDossierDest INTEGER PRIMARY KEY AUTOINCREMENT, 
---     cheminDossierDest TEXT NOT NULL
--- );
-
--- CREATE TABLE IF NOT EXISTS FichiersSource (
---     idFichier INTEGER PRIMARY KEY AUTOINCREMENT, 
---     idDossierSource INTEGER NOT NULL,     
---     nomFichier TEXT, 
---     extensionFichier TEXT, 
---     sizeFichier INTEGER, 
---     date_create_Fichier DATE, 
---     date_modif_Fichier DATE, 
---     CONSTRAINT fk_idDossierSource FOREIGN KEY(idDossierSource) REFERENCES DossiersSource(idDossierSource)
--- );
-
--- CREATE TABLE IF NOT EXISTS FichiersDestination (
---     idFichier INTEGER PRIMARY KEY AUTOINCREMENT, 
---     idDossierDest INTEGER NOT NULL, 
---     nomFichier TEXT, 
---     extensionFichier TEXT, 
---     sizeFichier INTEGER, 
---     date_create_Fichier DATE, 
---     date_modif_Fichier DATE, 
---     CONSTRAINT fk_idDossierDest FOREIGN KEY(idDossierDest) REFERENCES DossiersDestination(idDossierDest)
--- );
-
 CREATE TABLE IF NOT EXISTS DossiersSource (
     idDossierSource INTEGER PRIMARY KEY AUTOINCREMENT, 
-    cheminDossierSource TEXT NOT NULL, chemintest1 TEXT
+    cheminDossierSource TEXT NOT NULL,
+    chemintest1 TEXT
 );
 
 CREATE TABLE IF NOT EXISTS DossiersDestination (
     idDossierDest INTEGER PRIMARY KEY AUTOINCREMENT, 
-    cheminDossierDest TEXT NOT NULL, chemintest2 TEXT
+    cheminDossierDest TEXT NOT NULL,
+    chemintest2 TEXT
 );
 
 CREATE TABLE IF NOT EXISTS FichiersSource (
